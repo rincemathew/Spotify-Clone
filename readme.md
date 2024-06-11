0963dfd540c5396af234244acd8f993e914f8659
Rince Mathew <rincemathewmanattu@gmail.com>
2024-06-11 22:07:08 +0530

    mongodb connect

--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 
 nodemone 
 
-mongoose to connect mongdb
-
+mongoose to connect mongdb cluster
+create cluster and a user with permission
+using mongoose connection string with connect to database
 
 
 
--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const port = 8000
+const port = 8000;
+
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_DB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then((x)=>{
+    console.log("MongoDb connected.....")
+}).catch((error)=>{
+    console.log("MongoDB not Connected" + error.message)
+})
 
 app.get("/",(req,res)=>{
     res.send("hello world");
