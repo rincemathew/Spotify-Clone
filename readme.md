9c025f78761b2c27e7d358fdf33e19a4a4c0a507
Rince Mathew <rincemathewmanattu@gmail.com>
2024-06-12 10:53:54 +0530

    passport jwt

--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 
-nodemone 
+nodemon
 
 mongoose to connect mongdb cluster
 create cluster and a user with permission
 using mongoose connection string with connect to database
 
+
+passport jwt
 
 
 
--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const User = require("./models/user");
 const port = 8000;
 
 require("dotenv").config()
 
+//mongoose
 mongoose.connect(process.env.MONGO_DB,{
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -14,6 +19,29 @@ mongoose.connect(process.env.MONGO_DB,{
     console.log("MongoDB not Connected" + error.message)
 })
 
+//passport jwt
+
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secretkey';
+// opts.issuer = 'accounts.examplesoft.com';
+// opts.audience = 'yoursite.net';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
+
+//temp call
 app.get("/",(req,res)=>{
     res.send("hello world");
 });
