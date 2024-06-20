dc5d4a5f11e1c3e2a4cde5131bf7f100d3fc3700
Rince Mathew <rincemathewmanattu@gmail.com>
2024-06-20 22:22:55 +0530

    login page

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -1,14 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Login from './routes/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-screen h-screen">
        <BrowserRouter>
         <Routes>
           <Route path="/" element={<div>hello</div>} />
-          <Route path="/hai" element={<div>hai</div>} />
+          <Route path="/login" element={<Login></Login>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ spotify_frontend/src/routes/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Icon } from '@iconify/react';
+
+function Login() {
+  return (
+    <div className='w-full h-full flex flex-col items-center'>
+        <div className='p-6 border-b border-solid border-gray-400 w-full flex justify-center'>
+        <Icon icon="logos:spotify" width="150"/>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Login
